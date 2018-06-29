ea78eed84cccb71a9dbef4760b6a40208478b7bc
Maxi Clayton Clowes <maxi@codogo.io>
2018-06-29 15:12:31 +0100

    chore(handbooker): Upgraded and changed style option accordingly

--- scripts/createRulebooks.js
+++ scripts/createRulebooks.js
@@ -2,7 +2,6 @@ const { handbooker, } = require("handbooker");
 
 const options = {
 	"debug": true,
-	"style": "./node_modules/handbooker/lib/styles/homebrewery-styles.css",
 	"printOptions": {
 		displayHeaderFooter: false,
 	},
